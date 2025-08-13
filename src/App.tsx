1a13ed2a4f4227268036aed4f2e3b48ce7234d31
Benjamin Mehl <Benni.mehlBM@gmail.com>
2025-08-13 15:30:22 +0200

    supabase connected

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,28 @@ import { Toaster } from "@/components/ui/toaster";
 import Index from "./pages/Index";
 import GoalPage from "./pages/GoalPage";
 import "./App.css";
+import { useEffect } from "react";
+import supabase from "./lib/supabase";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/goal/:id" element={<GoalPage />} />
-      </Routes>
-      <Toaster />
-    </Router>
-  );
+    async function getTest() {
+        const { data: test } = await supabase.from("test").select();
+        console.log(test);
+    }
+
+    useEffect(() => {
+        getTest();
+    }, []);
+
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="/goal/:id" element={<GoalPage />} />
+            </Routes>
+            <Toaster />
+        </Router>
+    );
 }
 
 export default App;
